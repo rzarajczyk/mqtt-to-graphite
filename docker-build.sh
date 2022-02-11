7c7366aca8e2f6e6dec5186607f67cf74983a094
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-02-11 12:28:53 +0100

    - docker push  - log on startup

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-TAG=$1
-if [ -z $TAG ]; then
-    echo "TAG is required"
-    exit 1
-fi
-
+TAG=$(date '+%Y%m%d')
 docker build -t rzarajczyk/mqtt-to-graphite:$TAG .
 docker tag rzarajczyk/mqtt-to-graphite:$TAG rzarajczyk/mqtt-to-graphite:latest
+docker push rzarajczyk/mqtt-to-graphite:$TAG
+docker push rzarajczyk/mqtt-to-graphite:latest
