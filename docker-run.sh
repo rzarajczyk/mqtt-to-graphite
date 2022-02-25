137f57047d8073a0110825ecb63e0fd8e436590a
rafal.zarajczyk <rafal.zarajczyk@allegro.pl>
2022-02-25 23:56:13 +0100

    Removed IP

--- docker-run.sh
+++ docker-run.sh
@@ -3,8 +3,6 @@ TMP=$(mktemp -d)
 cp $(pwd)/config/* $TMP
 echo "Temp directory is $TMP"
 docker run -it --rm  \
-    --add-host "graphite:192.168.86.159" \
-    --add-host "mosquitto:192.168.86.159" \
     --name mqtt-to-graphite \
     -v $TMP:/mqtt-to-graphite/config \
     rzarajczyk/mqtt-to-graphite:latest
