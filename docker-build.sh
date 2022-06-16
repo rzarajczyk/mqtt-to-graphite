5719a1159f67c37967374311a47e6c568c88dfcf
Rafal Zarajczyk <rzarajczyk@gmail.com>
2022-06-16 22:16:52 +0200

    building via github actions

--- docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TAG=$(date '+%Y%m%d')
-docker build -t rzarajczyk/mqtt-to-graphite:$TAG .
-docker tag rzarajczyk/mqtt-to-graphite:$TAG rzarajczyk/mqtt-to-graphite:latest
-docker push rzarajczyk/mqtt-to-graphite:$TAG
-docker push rzarajczyk/mqtt-to-graphite:latest
